8ab0b1f561d259f43382a387244dc55313948fdf
Ricardo <ricardo@ricardofideles.com.br>
2018-07-16 10:56:39 -0300

    Adding user repository

---
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -0,0 +1,19 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.demo.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	User findByName(String name);
+	
+	User findByEmail(String email);
+	
+	@Query("select u from User u where u.name like %?1%")
+	List<User> findUsersNameLike(String name);
+
+}
